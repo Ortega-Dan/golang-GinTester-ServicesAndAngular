eafa53df403c8b579a1413400f4b48cba232858d
Dan Ortega <ortega.dan2010@gmail.com>
2021-04-15 17:35:49 -0500

    Adding serving port

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// don't forget to add the last "/" at the end of the path
 var staticDir string = "../../typescript/ePortfolio/dist/ePortfolio/"
 
 func main() {
@@ -15,19 +16,23 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 
-	// Serving static content
+	// Using Gin Recovery AND Serving static content
 	r.Use(gin.Recovery(), static.Serve("/", static.LocalFile(staticDir, false)))
+	// Enabling angular router
 	r.NoRoute(func(c *gin.Context) {
 		c.File(staticDir + "index.html")
 	})
 
 	// Adding endpoints
 	r.GET("/ping", func(c *gin.Context) {
+
+		// panic("worng")
+
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
 	// running it
-	r.Run()
+	r.Run(":8023")
 }
